710d59c61eea71ed84908ddb52a166912b0a02c1
CuconuDenisa <56970679+CuconuDenisa@users.noreply.github.com>
2019-11-24 00:32:45 +0200

    Update pacientinserat.php

--- pacientinserat.php
+++ pacientinserat.php
@@ -46,7 +46,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=pie", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "select codpacient,nume,prenume,cnp,strada,numarstrada,datanasterii,sex,telefon,email,parola from pacient";
+	$sql = "select codpacient,nume,prenume,cnp,strada,numarstrada,datanasterii,gen,telefon,email,parola from pacient";
 	
     echo ""; 
     }
@@ -66,7 +66,7 @@ catch(PDOException $e)
 							echo "<td>".$row['strada']."</td>";
 								echo "<td>".$row['numarstrada']."</td>";
 									echo "<td>".$row['datanasterii']."</td>";
-										echo "<td>".$row['sex']."</td>";
+										echo "<td>".$row['gen']."</td>";
 											echo "<td>".$row['telefon']."</td>";
 												echo "<td>".$row['email']."</td>";
 													echo "<td>".$row['parola']."</td>";
@@ -79,7 +79,8 @@ catch(PDOException $e)
  <br>
 <form action="pacientinserat.php" method="post">
 	
-		<h1> Inserarea unui nou pacient</h1>	
+		<h1> Inserarea unui nou pacient</h1>
+		
 		Nume:&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<input type="text" name="nume" style="margin-right: 45 px">	
 		Prenume:<input type="text" name="prenume" style="margin-right: 45 px">
@@ -90,13 +91,14 @@ catch(PDOException $e)
 		Numarul Strazii:<input type="text" name="numarstrada" style="margin-right: 45 px">
 		<br>
 		Data nasterii:<input type="text" name="datanasterii" style="margin-right: 45 px">		
-		Sex:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
-		<input type="text" name="sex" style="margin-right: 45 px">
+		Gen:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+		<input type="text" name="gen" style="margin-right: 45 px">
 		Telefon:&nbsp;&nbsp;<input type="text" name="telefon" style="margin-right: 45 px">		
 		Email:<input type="text" name="email" style="margin-right: 45 px">
 		Parola: &nbsp; &nbsp;<input type="text" name="parola" style="margin-right: 45 px">		
 		&nbsp; &nbsp; 
 		<input style="background-color:white;color:black;width:70px;" type="submit" name="Adaugati_pacient" class="button" value="Inserează"/>	
+
 		<br/>
 		</form>
 <?php
@@ -113,19 +115,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$strada=$_POST['strada'];
 		$numarstrada=$_POST['numarstrada'];
 		$datanasterii=$_POST['datanasterii'];
-		$sex=$_POST['sex'];
+		$gen=$_POST['gen'];
 		$telefon=$_POST['telefon'];
 		$email=$_POST['email'];
 		$parola=$_POST['parola'];
-		$sql="INSERT INTO pacient (codpacient, nume, prenume, oras, cnp, strada, numarstrada, datanasterii, sex, telefon, email, parola)
+		$sql="INSERT INTO pacient (codpacient, nume, prenume, oras, cnp, strada, numarstrada, datanasterii, gen, telefon, email, parola)
     VALUES (default,?,?,?,?,?,?,?,?,?,?,?)";
 		$result =$dbx->prepare($sql);
-		$count= $result->execute(array($nume,$prenume,$oras,$cnp,$strada,$numarstrada,$datanasterii,$sex,$telefon,$email,$parola));
+		$count= $result->execute(array($nume,$prenume,$oras,$cnp,$strada,$numarstrada,$datanasterii,$gen,$telefon,$email,$parola));
 		echo '<script language="javascript">';
 		echo 'alert("Pacient inserata cu succes")';
 		echo'</script>';
 	 }
 }
 ?>
+<?php
+if(isset($_POST['Stergeti_pacient'])){
+	try{
+		$pdoConnect = new PDO("mysql:host=localhost;dbname=pie","root","");
+	}catch(PDOException $EXC){
+		echo $exc->getMessage();
+		exit();
+	}
+	$codpacient =$_POST['codpacient'];
+	$pdoQuery="DELETE FROM pacient where codpacient=:codpacient";
+	$pdoResult= $pdoConnect->prepare($pdoQuery);
+	$pdoExec = $pdoResult->execute(array(":codpacient"=>$codpacient));
+	echo '<script language="javascript">';
+	echo 'alert("Pacientul a fost  stears cu succes")';
+	echo'</script>';
+	
+}
+?>
+<form action="pacientinserat.php" method="post">
+		<br>Cod Pacient:<input type="text" name="codpacient" required>
+		<input style="background-color:white;color:black;width:70px;" type="submit" name="Stergeti_pacient" class="button" value="Șterge"/>	
+</form>
+<br>
+
+	Diagrama:
+	 <form action="diagrama.php" class="inline">
+    <button class="float-left submit-button" style="background-color:white;color:black;width:70px;">Diagrama</button>
+</form>
+<br>
+Afisare Istoric Internari:
+	 <form action="istoricpacienti.php" class="inline">
+    <button class="float-left submit-button" style="background-color:white;color:black;width:70px;">Istoric</button>
+</form>
 </body>
 </html>
+
