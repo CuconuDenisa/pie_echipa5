166f1c4ef9679deb5f96615ab0859850ad954b02
CuconuDenisa <56970679+CuconuDenisa@users.noreply.github.com>
2020-01-11 22:00:12 +0200

    Add files via upload

---
+++ programari.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Meniu Doctori </title>
+</head>
+<body>
+<form action="programari.php" method="post">
+	<h1> PROGRAMARE ONLINE:</h1>	
+		Nume Complet:
+		<input type="text" name="nume" style="margin-right: 45 px">	
+		<br>
+		Secializarea:
+		<input type="text" name="specializarea" style="margin-right: 45 px">
+		<br>
+		Numar de telefon:
+		<input type="text" name="telefon" style="margin-right: 45 px">
+		<br>
+		Email:
+		<input type="text" name="email" style="margin-right: 45 px">
+		<br>
+		Preferinte Medic:
+		<input type="text" name="medic" style="margin-right: 45 px">							
+		<br>
+		<input style="background-color:white;color:black;width:70px;" type="submit" name="trimite" class="button" value="trimite"/>	
+		</form>
+</body>
+</html>
+<?php
+include_once 'conexiune.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	 if(isset($_POST['trimite'])){
+		$database = new conexiune();
+		$dbx =$database->openConnection();
+		$nume=$_POST['nume'];
+		$specializarea=$_POST['specializarea'];
+		$telefon=$_POST['telefon'];
+		$email=$_POST['email'];
+		$medic=$_POST['medic'];
+		$sql="INSERT INTO rezervari(codrezervare,nume,specializarea,telefon,email,medic)
+		VALUES(default,?,?,?,?,?)";
+		$result =$dbx->prepare($sql);
+		$count= $result->execute(array($nume,$specializarea,$telefon,$email,$medic));
+		echo '<script language="javascript">';
+		echo 'alert("Rezervare cu succes")';
+		echo'</script>';
+	 }
+}
+?>
